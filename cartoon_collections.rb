0c5c12fbf9a2188568985a99b9e4dace9687f108
Brandon Chin <brandonchin@me.com>
2018-02-14 21:36:38 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,3 @@ def long_planeteer_calls(array)
     word.length > 4
   end
 end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
