88bfb38ef5423a6f17d6880ae7b31277db2ea84e
Brandon Chin <brandonchin@me.com>
2018-02-14 21:40:34 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,10 +11,4 @@ def summon_captain_planet(array)
   end
 end
 
-def long_planeteer_calls(array)
-end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
