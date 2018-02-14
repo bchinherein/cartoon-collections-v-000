4721e72a4eb7d6aee062a723cdb8c03c41292f02
Brandon Chin <brandonchin@me.com>
2018-02-14 22:03:50 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  
   array.find do |cheese|
     cheese == "cheddar" || "gouda" || "camembert"
   end
