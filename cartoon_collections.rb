b01b3899ff9209397fc3193ba080cdfa886b859e
Brandon Chin <brandonchin@me.com>
2018-02-14 21:28:10 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,10 +13,7 @@ end
 
 def long_planeteer_calls(array)
   array.any? do |word|
-    if word.length > 4
-      true
-    end
-  end
+    word.length > 4
 end
 
 def find_the_cheese# code an argument here
