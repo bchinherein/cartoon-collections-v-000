058642ac427a6b9d4fbf7dbbbd398961ff128e77
Brandon Chin <brandonchin@me.com>
2018-02-14 21:42:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,3 +9,14 @@ def summon_captain_planet(array)
     uppercase.capitalize + "!"
   end
 end
+
+def long_planeteer_calls(array)
+  array.any? do |word|
+    word.length > 4
+  end
+end
+
+def find_the_cheese# code an argument here
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
+end
